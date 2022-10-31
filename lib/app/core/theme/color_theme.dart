8f800e66e4171c77ee410d3010905abacd5d1fe9
Minyong Oh <dhalsdyd@gmail.com>
2022-10-31 10:04:02 +0900

    add text,color component

--- lib/app/core/theme/color_theme.dart
+++ lib/app/core/theme/color_theme.dart
@@ -1,6 +1,20 @@
+// ignore_for_file: constant_identifier_names
+
 import 'package:flutter/material.dart';
 
 abstract class FGBPColors {
+  static const Black1 = Color(0xFF000000);
+  static const Black2 = Color(0xFF383838);
+  static const Black3 = Color(0xFF828282);
+  static const Black4 = Color(0xFFD1D1D1);
+
+  static const White1 = Color(0xFFFFFFFF);
+
+  static const Brown1 = Color(0xFF897A63);
+  static const Brown2 = Color(0xFF9F8772);
+  static const Brown3 = Color(0xFFD4B9A0);
+  static const Brown4 = Color(0xFFD8D3CE);
+
   /// Main Color
   static const mainColor = Colors.amber;
 
--- lib/app/core/theme/text_theme.dart
+++ lib/app/core/theme/text_theme.dart
@@ -2,6 +2,67 @@ import 'package:backup/app/core/theme/color_theme.dart';
 import 'package:flutter/material.dart';
 
 abstract class FGBPTextTheme {
+  static const head1 = TextStyle(
+    color: FGBPColors.Black1,
+    fontSize: 28,
+    fontWeight: FontWeight.w700,
+  );
+  static const head2 = TextStyle(
+    color: FGBPColors.Black1,
+    fontSize: 20,
+    fontWeight: FontWeight.w700,
+  );
+  static const head3 = TextStyle(
+    color: FGBPColors.Black1,
+    fontSize: 16,
+    fontWeight: FontWeight.w700,
+  );
+  static const head4 = TextStyle(
+    color: FGBPColors.Black1,
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const text1 = TextStyle(
+    color: FGBPColors.Black1,
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+  );
+  static const text1Bold = TextStyle(
+    color: FGBPColors.Black1,
+    fontSize: 12,
+    fontWeight: FontWeight.w700,
+  );
+  static const text2 = TextStyle(
+    color: FGBPColors.Black1,
+    fontSize: 14,
+    fontWeight: FontWeight.w300,
+  );
+  static const text2Bold = TextStyle(
+    color: FGBPColors.Black1,
+    fontSize: 14,
+    fontWeight: FontWeight.w700,
+  );
+  static const text3 = TextStyle(
+    color: FGBPColors.Black1,
+    fontSize: 16,
+    fontWeight: FontWeight.w300,
+  );
+  static const text3Bold = TextStyle(
+    color: FGBPColors.Black1,
+    fontSize: 16,
+    fontWeight: FontWeight.w700,
+  );
+  static const text4 = TextStyle(
+    color: FGBPColors.Black1,
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+  );
+  static const text4Bold = TextStyle(
+    color: FGBPColors.Black1,
+    fontSize: 16,
+    fontWeight: FontWeight.w700,
+  );
   //Default Black
   //Name Rule
   //FontWeight + Color + Font Size
