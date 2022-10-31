2f8ff5a4a152232378acc4ee7f8c9ccfadb1acbc
Minyong Oh <dhalsdyd@gmail.com>
2022-10-31 10:46:26 +0900

    Apply splash page

--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,19 @@ import 'package:backup/app/routes/pages.dart';
 import 'package:backup/app/routes/route.dart';
 import 'package:backup/app/translation/translation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 //import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
 
 void main() async {
   //WidgetsBinding widgetsBinding =
-  WidgetsFlutterBinding.ensureInitialized();
-  // FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
-  // loading
-  // FlutterNativeSplash.remove(widgetsBinding: widgetsBinding);
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  await AppInitalizer().init();
+  FlutterNativeSplash.remove();
 
   // Do Firebase Initial Settings With Firebase CLI
   // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  await AppInitalizer().init();
   //Get.config();
   runApp(
     GetMaterialApp(
