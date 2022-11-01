72d9957371e455b72c54ae178936f51b5464747f
Minyong Oh <dhalsdyd@gmail.com>
2022-11-01 17:53:15 +0900

    Update Drawer Design

--- lib/app/pages/home/widget/drawer.dart
+++ lib/app/pages/home/widget/drawer.dart
@@ -1,6 +1,8 @@
 import 'package:backup/app/core/theme/text_theme.dart';
+import 'package:backup/app/widgets/button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
 
 class MainDrawer extends StatelessWidget {
   const MainDrawer({Key? key}) : super(key: key);
@@ -11,16 +13,28 @@ class MainDrawer extends StatelessWidget {
       child: Container(
         padding: const EdgeInsets.symmetric(vertical: 8),
         child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            SvgPicture.asset(
-              icon,
-              width: 24,
-              height: 24,
+            Row(
+              children: [
+                SvgPicture.asset(
+                  icon,
+                  width: 24,
+                  height: 24,
+                ),
+                const SizedBox(width: 16),
+                Text(
+                  text,
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ],
             ),
-            const SizedBox(width: 16),
-            Text(
-              text,
-              style: const TextStyle(
+            const Text(
+              ">",
+              style: TextStyle(
                 fontSize: 16,
                 fontWeight: FontWeight.w500,
               ),
@@ -34,6 +48,11 @@ class MainDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          bottomRight: Radius.circular(30),
+        ),
+      ),
       child: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(35),
@@ -47,6 +66,8 @@ class MainDrawer extends StatelessWidget {
               _menuItem("assets/icons/pattern.svg", "Family Code", () {}),
               _menuItem("assets/icons/spectacle.svg", "Manage Family", () {}),
               _menuItem("assets/icons/Insights.svg", "Request Album", () {}),
+              Expanded(child: Container()),
+              const Text("Smile DeveloperS", style: FGBPTextTheme.description),
             ],
           ),
         ),
@@ -58,19 +79,32 @@ class MainDrawer extends StatelessWidget {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        Container(
-          width: 48,
-          height: 48,
-          decoration: BoxDecoration(
-            color: Colors.grey,
-            borderRadius: BorderRadius.circular(8),
-          ),
-        ),
-        Column(
-          children: const [
-            Text("EXAMPLE", style: FGBPTextTheme.text1Bold),
-            Text("@EXAMPLE", style: FGBPTextTheme.text1),
+        Row(
+          children: [
+            Container(
+              width: 48,
+              height: 48,
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.circular(8),
+              ),
+            ),
+            const SizedBox(width: 4),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text("EXAMPLE", style: FGBPTextTheme.text1Bold),
+                Text("@EXAMPLE",
+                    style: FGBPTextTheme.text1.copyWith(fontSize: 12)),
+              ],
+            ),
           ],
+        ),
+        FGBPIconButton(
+          "assets/icons/cross.svg",
+          onTap: () {
+            Get.back();
+          },
         ),
       ],
     );
